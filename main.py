8a8be78ff22f7b5e18d40efb3359a3bbd2ad1e18
Adam Toth <tothadam500@gmail.com>
2023-07-29 09:09:56 +0200

    add model code as comment

--- main.py
+++ main.py
@@ -22,6 +22,27 @@ training_labels = training_labels[:20000]
 testing_images = testing_images[:4000]
 testing_labels = testing_labels[:4000]
 
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+# model.add(layers.MaxPooling2D((2,2)))
+# model.add(layers.Conv2D(64, (3,3), activation='relu'))
+# model.add(layers.MaxPooling2D((2,2)))
+# model.add(layers.Conv2D(64, (3,3), activation='relu'))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(10, activation='softmax'))
+#
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+#
+# model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
+#
+# loss, accuracy = model.evaluate(testing_images, testing_labels)
+# print(f'Loss: {loss}')
+# print(f'Accuracy: {accuracy}')
+#
+# model.save('image_classifier.model')
+
+
 model = models.load_model('image_classifier.model')
 
 for img in ['horse', 'car', 'plane']:
